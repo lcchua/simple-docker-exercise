37adb3b20084387eaba787de91e00ad1de34a96a
lcchua <laichwang@gmail.com>
2024-09-19 13:05:48 +0800

    Updated app_v1 with another new Hello message

--- app_v1.py
+++ app_v1.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 #	message = "Hello Mary to the world! (app_v1)"
-	message = "Hello Mary to the universe! (app_v1)"
+#	message = "Hello Mary to the universe! (app_v1)"
+	message = "Hello Mary to the galaxy! (app_v1)"
 	return message
 
 if __name__ == '__main__':
